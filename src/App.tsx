815bad1c7c665878121d5c7d5411e4277a148964
Shuveloper <shuveloper0308@gmail.com>
2025-05-19 22:09:18 +0900

    feat: Complete calculator

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,189 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  const [value, setValue] = useState("");
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <div className="calculator">
+        <form action="">
+          <div className="display">
+            <input type="text" value={value} />
+          </div>
+          <div>
+            <input type="button" value="AC" onClick={() => setValue("")} />
+            <input
+              type="button"
+              value="DE"
+              onClick={(e) => setValue(value.slice(0, -1))}
+            />
+            <input
+              type="button"
+              value="."
+              onClick={(e) =>
+                setValue((prev) => {
+                  return prev + (e.target as HTMLInputElement).value;
+                })
+              }
+            />
+            <input
+              type="button"
+              value="/"
+              onClick={(e) =>
+                setValue((prev) => {
+                  return prev + (e.target as HTMLInputElement).value;
+                })
+              }
+            />
+          </div>
+          <div>
+            <input
+              type="button"
+              value="7"
+              onClick={(e) =>
+                setValue((prev) => {
+                  return prev + (e.target as HTMLInputElement).value;
+                })
+              }
+            />
+            <input
+              type="button"
+              value="8"
+              onClick={(e) =>
+                setValue((prev) => {
+                  return prev + (e.target as HTMLInputElement).value;
+                })
+              }
+            />
+            <input
+              type="button"
+              value="9"
+              onClick={(e) =>
+                setValue((prev) => {
+                  return prev + (e.target as HTMLInputElement).value;
+                })
+              }
+            />
+            <input
+              type="button"
+              value="*"
+              onClick={(e) =>
+                setValue((prev) => {
+                  return prev + (e.target as HTMLInputElement).value;
+                })
+              }
+            />
+          </div>
+          <div>
+            <input
+              type="button"
+              value="4"
+              onClick={(e) =>
+                setValue((prev) => {
+                  return prev + (e.target as HTMLInputElement).value;
+                })
+              }
+            />
+            <input
+              type="button"
+              value="5"
+              onClick={(e) =>
+                setValue((prev) => {
+                  return prev + (e.target as HTMLInputElement).value;
+                })
+              }
+            />
+            <input
+              type="button"
+              value="6"
+              onClick={(e) =>
+                setValue((prev) => {
+                  return prev + (e.target as HTMLInputElement).value;
+                })
+              }
+            />
+            <input
+              type="button"
+              value="+"
+              onClick={(e) =>
+                setValue((prev) => {
+                  return prev + (e.target as HTMLInputElement).value;
+                })
+              }
+            />
+          </div>
+          <div>
+            <input
+              type="button"
+              value="1"
+              onClick={(e) =>
+                setValue((prev) => {
+                  return prev + (e.target as HTMLInputElement).value;
+                })
+              }
+            />
+            <input
+              type="button"
+              value="2"
+              onClick={(e) =>
+                setValue((prev) => {
+                  return prev + (e.target as HTMLInputElement).value;
+                })
+              }
+            />
+            <input
+              type="button"
+              value="3"
+              onClick={(e) =>
+                setValue((prev) => {
+                  return prev + (e.target as HTMLInputElement).value;
+                })
+              }
+            />
+            <input
+              type="button"
+              value="-"
+              onClick={(e) =>
+                setValue((prev) => {
+                  return prev + (e.target as HTMLInputElement).value;
+                })
+              }
+            />
+          </div>
+          <div>
+            <input
+              type="button"
+              value="00"
+              onClick={(e) =>
+                setValue((prev) => {
+                  if (prev !== "") {
+                    return prev + (e.target as HTMLInputElement).value;
+                  } else return "";
+                })
+              }
+            />
+            <input
+              type="button"
+              value="0"
+              onClick={(e) =>
+                setValue((prev) => {
+                  if (prev !== "") {
+                    return prev + (e.target as HTMLInputElement).value;
+                  } else return "";
+                })
+              }
+            />
+            <input
+              type="button"
+              value="="
+              className="equal"
+              onClick={(e) => setValue(eval(value))}
+            />
+          </div>
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
